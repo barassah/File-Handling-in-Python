7db6a23fd6a2e965c4f1c1d85f66c47bbe2eb322
Barassah <barassah@gmail.com>
2019-08-10 16:18:46 +0300

    Update F_handling.py

--- F_handling.py
+++ F_handling.py
@@ -3,37 +3,37 @@
 import os
 #checking if file exists before creating 
 if os.path.exists('text.txt'):
-#	print('File Exist!')
+	#print('File Exist!')
 	#inquire overwrite or append content
 	#prompt for over witing of file contents
 	i=input('Overwrite file? y/n: ')
-	if i!=n:
-		myfile = open('text.txt', 'a') 
-		#appending text to be done
-		myfile.write('\nHello there, added Content!')
-		myfile.close()
-	else:
+	if i!='n':
 		myfile = open('text.txt', 'w') 
 		#appending text to be done
-		myfile.write('Hello there, New Content!')
+		myfile.write('Hello there, added Content!')
+		myfile.close()
+	else:
+		myfile = open('text.txt', 'a') 
+		#appending text to be done
+		myfile.write('\nHello there, New Content!')
 		myfile.close()
 else:
-#	print('File Doesn\'t Exist')
+	#print('File Doesn\'t Exist')
 	#create a new and opening the file to append new content
 	myfile = open('text.txt', 'a') 
 	#appending text to be done
 	myfile.write('Hello there, New Content!')
 	#close file
 	myfile.close()
-#opening file to read
-f=open('text.txt','r')
+#open and read file created
+myfile=open('text.txt','r+')
 #read one line using the readdline function
 #print(f.readline())
 #Read and display the entire content in the text file
-for x in f:
+for x in myfile:
 	print(x)
 #close the file
-f.close()
+myfile.close()
 #prompt for deletion of file
 d=input('Delete File? y/n: ')
 if d!='n':
